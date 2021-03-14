3acf38eb6f8510d6c86633717d531f9abc872326
sujay <sujaypp@gmail.com>
2021-03-14 20:47:15 +0530

    EOS

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,8 @@ namespace API
 
             });
 
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +51,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(policy =>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
